e6abc5b79aba364a5c8a1358a1c97a3a7961ed90
jesus <jmba0190@gmail.com>
2024-09-10 19:41:37 -0500

    added div method

--- src/homework/GitHomework.java
+++ src/homework/GitHomework.java
@@ -3,20 +3,16 @@ package homework;
 public class GitHomework {
 	
 	public static void main(String[] args) {
-		
-		
 		System.out.println("Hello world");
-		
-		
-		
 	}
 	
 	
 	public static void add(int a, int b) {
-		
-		
 		System.out.println(a+b);
-		
 	}
 
+	public static void div(int a, int b) {
+		System.out.println(a/b);
+	}
+	
 }

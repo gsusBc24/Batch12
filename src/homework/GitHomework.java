38399af708cf1cf48042b0664197bd8a5e88cb48
jesus <jmba0190@gmail.com>
2024-09-10 20:15:17 -0500

    new empty

--- src/homework/GitHomework.java
+++ src/homework/GitHomework.java
@@ -2,33 +2,8 @@ package homework;
 
 public class GitHomework {
 	
-	public static void main(String[] args) {
-		System.out.println("Hello world");
-	int age = 12;
-	String name = "Jhon";
-		//This came from Github
 
-		System.out.println("name is: "+name +" age: "+ age);
-		
-	}
-	
-	
-	public static void add(int a, int b) {
-		System.out.println(a+b);
-	}
 
-	public static void div(int a, int b) {
-		System.out.println(a/b);
-	}
-	
-	public static void multi(int a, int b) {
-		System.out.println(a*b);
-	}
-	
-	public static void substract(int a, int b) {
-		System.out.println(a-b);
-	}
-	
 	
 }
 

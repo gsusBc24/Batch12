7b7af4d272ec5819391ffc2a0e1552ee27737cb8
jesus <jmba0190@gmail.com>
2024-09-10 19:35:30 -0500

    first move

--- src/homework/GitHomework.java
+++ src/homework/GitHomework.java
@@ -5,9 +5,17 @@ public class GitHomework {
 	public static void main(String[] args) {
 		
 		
+		System.out.println("Hello world");
 		
 		
 		
+	}
+	
+	
+	public static void add(int a, int b) {
+		
+		
+		System.out.println(a+b);
 		
 	}
 
